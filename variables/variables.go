8c7a78d389b1bf1fcf10ac6f8e72994f51365eb9
Mauro <mrtelefonia@gmail.com>
2020-12-10 11:47:45 -0300

    variables

---
+++ variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "John doe"
+	age := 35
+	status := true
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(status)
+}
+
+//go run <name.go>
